b2eff8be8ec407510e6a13ed668d276b47d07cc2
faii <1320407956@qq.com>
2024-06-20 17:46:07 +0800

    AUC_version

--- src/RNN.py
+++ src/RNN.py
@@ -4,23 +4,19 @@ from RNN_model import RNNClassifier
 import torch
 import torch.nn as nn
 from torch.optim import Adam
-
+from sklearn.metrics import roc_auc_score
 
 train_dataloader, test_dataloader, vocab = get_data_loaders(batch_size=8)
 
-
-num_classes = 1  
-vocab_size = len(vocab) 
+num_classes = 1 
+vocab_size = len(vocab)  
 embed_dim = 64
 hidden_dim = 128
 
-
 model = RNNClassifier(vocab_size, embed_dim, hidden_dim, num_classes)
-
 
 criterion = nn.BCEWithLogitsLoss()
 optimizer = Adam(model.parameters(), lr=0.001)
-
 
 def train(dataloader, model, criterion, optimizer):
     model.train()
@@ -36,24 +32,29 @@ def train(dataloader, model, criterion, optimizer):
         total_count += label.size(0)
     return total_acc/total_count
 
-
-def evaluate(dataloader, model):
+def evaluate(dataloader, model, criterion):
     model.eval()
     total_acc, total_count = 0, 0
+    all_predictions = []
+    all_labels = []
     with torch.no_grad():
         for idx, (text, label) in enumerate(dataloader):
             predicted_label = model(text)
-            total_acc += ((predicted_label.squeeze(1) > 0.5) == label).sum().item()
+            loss = criterion(predicted_label.squeeze(1), label)
+            probabilities = torch.sigmoid(predicted_label).squeeze(1)
+            all_predictions.extend(probabilities.cpu().numpy())
+            all_labels.extend(label.cpu().numpy())
+            total_acc += ((probabilities > 0.5) == label).sum().item()
             total_count += label.size(0)
-    return total_acc/total_count
-
+    auc = roc_auc_score(all_labels, all_predictions)
+    return total_acc/total_count, auc
 
 if __name__ == "__main__":
     print('Train Start')
-    num_epochs = 10  # 训练轮次
+    num_epochs = 10  
     for epoch in range(num_epochs):
         accu_train = train(train_dataloader, model, criterion, optimizer)
         print(f'Epoch: {epoch+1}, Train accuracy: {accu_train:.2f}')
         
-        accu_test = evaluate(test_dataloader, model)
-        print(f'Test accuracy: {accu_test:.2f}')
+        accu_test, auc_test = evaluate(test_dataloader, model, criterion)
+        print(f'Test accuracy: {accu_test:.2f}, Test AUC: {auc_test:.2f}')
